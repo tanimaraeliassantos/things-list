a74520e3e3032d740ea4777acbb4d71cee252b36
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-03-03 11:50:01 +1100

    Create app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  function addItem() {
+    setThingsArray((prevThingsArray) => {
+      return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`];
+    });
+  }
+
+  const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+
+  return (
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      {thingsElements}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
